04339698e2f493e12129d33211316f452a0ff876
Claudia Lam <claudiaslam@gmail.com>
2023-06-27 16:34:46 -0700

    add coordinates to each cell

--- static/boggle.js
+++ static/boggle.js
@@ -8,7 +8,6 @@ const $table = $("table");
 
 let gameId;
 
-
 /** Start */
 
 async function start() {
@@ -22,8 +21,22 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+
+  //loop over the td
+  for (let y = 0; y < board.length; y++) {
+    const $row = $("<tr>")
+    for (let x = 0; x < board[0].length; x++) {
+      //add coordinates to each td as a class
+      const $cell = $("<td>")
+      $cell.addClass(`${x}-${y}`)
+      $row.append($cell)
+        //add text to each td
+      }
+    $table.append($row)
+  }
+
 }
 
 
